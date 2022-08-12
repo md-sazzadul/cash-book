90831463eb434f52c85c72469a86bda308ba672d
md-sazzadul <md.sazzadul.islam15@gmail.com>
2022-08-12 12:38:09 +0200

    conditions added

--- js/app.js
+++ js/app.js
@@ -1,29 +1,59 @@
+function expenseCalculation(foodExpense, rentExpense, clothesExpense){
+    const expenseOutput = parseInt(foodExpense) + parseInt(rentExpense) + parseInt(clothesExpense);
+    return expenseOutput;
+}
+
+function balanceCalculation(income, expenses){
+    const totalBalance = parseInt(income) - expenses;
+    return totalBalance;
+}
+
+function savingAmountCalculation(income, savingsPercent){
+    const savingAmount = parseInt(income) * parseInt(savingsPercent)/100;
+    return savingAmount;
+}
+
+function remainingBalanceCalculation(totalBalance, savingAmount){
+    const remainingBalance = totalBalance - savingAmount;
+    return remainingBalance;
+}
+
 document.getElementById('calculate-btn').addEventListener('click', function(){
+    // getting value of income
+    const income = document.getElementById('income-field').value;
+
     // getting value of expenses
     const foodExpense = document.getElementById('food-field').value;
     const rentExpense = document.getElementById('rent-field').value;
     const clothesExpense = document.getElementById('clothes-field').value;
 
     // show the total expenses
-    const expenseOutput = parseInt(foodExpense) + parseInt(rentExpense) + parseInt(clothesExpense);
-    document.getElementById('total-expense').innerText = expenseOutput;
-
-    // getting value of income
-    const income = document.getElementById('income-field').value;
+    let totalExpenses;
+    if (foodExpense > 0 && rentExpense > 0 && clothesExpense > 0){
+        totalExpenses = expenseCalculation(foodExpense, rentExpense, clothesExpense);
+    }
+    document.getElementById('total-expense').innerText = totalExpenses;
     
-    // show the total balance 
-    const totalBalanceField = document.getElementById('total-balance');
-    totalBalanceField.innerText = parseInt(income) - expenseOutput;
+    // show the total balance
+    let totalBalance;
+    if (income > 0){
+        totalBalance = balanceCalculation(income, totalExpenses);
+    }
+    document.getElementById('total-balance').innerText = totalBalance;
 
     document.getElementById('save-btn').addEventListener('click', function(){
         // getting value of saving percentage
         const savingsPercent = document.getElementById('savings-field').value;
-        
+
         // show the savings amount
-        const savingField = document.getElementById('saving-amount');
-        savingField.innerText = income * parseInt(savingsPercent)/100;
+        let savingAmount;
+        if (savingsPercent > 0 && savingsPercent <= 100){
+            savingAmount = savingAmountCalculation(income, savingsPercent);
+        }
+        document.getElementById('saving-amount').innerText = savingAmount;
 
         // show the remaining balance 
-        document.getElementById('remaining-balance').innerText = totalBalanceField.innerText - savingField.innerText;
+        const remainingBalance = remainingBalanceCalculation(totalBalance, savingAmount);
+        document.getElementById('remaining-balance').innerText = remainingBalance;
     })
 });
